0c9c8e65b5aaa66a1395d83e0f4ace77b970a4d7
Zhicheng Gong <109425to@gmail.com>
2026-03-07 19:04:51 +0800

    try fix: transform proxy to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(req: NextRequest) {
+export const runtime = "edge";
+
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const access_token = req.cookies.get("lyps_access_token");
 
